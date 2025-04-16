b77331b154d7727f55f9cb2411456e83e7800feb
adamnmartinez <adam.nmartinez@gmail.com>
2025-04-16 12:00:11 -0700

    Created barebones server with basic methods to bounce responses to backend

--- API/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-
-/* 
-
-TODO: See Notion
-
-*/
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+const PORT = 8080
+
+app.listen(PORT, () => {
+    console.log(`Server initalized on port ${PORT}`)
+    // TODO: Get Database Connection
+})
+
+app.get("/", (req, res) => {
+    return res.status(200).json({
+        message: "PinPoint API server is running."
+    })
+})
+
+app.post("/api/login", (req, res) => {
+    const { username } = req.body
+    const { password } = req.body
+
+    return res.status(200).json({
+        message: `Authenticated User! (${username})`,
+        user: username,
+        token: "randomtoken"
+    })
+})
+
+app.post("/api/signup", (req, res) => {
+    const { email } = req.body
+    const { username } = req.body
+    const { password } = req.body
+
+    return res.status(200).json({
+        message: `Registering User... (${username}, ${email})`,
+        user: username,
+        email : email,
+        token: "randomtoken"
+    })
+})
