ef2b60124e7110c6aed90b8d69cd28423c8e23d1
Larry <lunalarry143@gmail.com>
2025-05-12 22:11:19 -0700

    added user insert, find all users that endorsed

--- queryFunctions.tsx
+++ queryFunctions.tsx
@@ -1,3 +1,31 @@
+/*import express, { Router } from 'express';
+import {pool} from "./test.js"
+
+const express = require("express");
+const app = express();
+const taskRouter = Router();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+//taskRouter.get('/tasks', getTasks);
+*/
+
+app.post('/tasks/addUser', async(req, res) =>{
+  const { user }  = req.body
+  try{
+    const query = {
+      text: "INSERT INTO validity (uid) VALUES ($1)",
+      values: [user]
+    }
+    const db_res = await pool.query(query);
+    return res.status(201).json({ message: "Endorsed pin" });
+  }catch (e){
+    console.log("Unable to complete request")
+    console.log(e)
+    return res.status(500).json({ message: "internal server error.", error: e})
+  }
+})
+
 app.post('/tasks/add', async(req, res) =>{
   const { user }  = req.body
   const { pin } = req.body
@@ -31,4 +59,25 @@ app.post('/tasks/delete', async(req, res) =>{
     console.log(e)
     return res.status(500).json({ message: "internal server error.", error: e})
   }
+})
+
+app.get('/tasks/validates/:id', async(req, res)=>{
+  const pid = req.params['id']
+  const query = {
+    text: 'SELECT * FROM validity WHERE $1 = ANY(endorsed_pins)',
+    values: [pid]
+  }
+  console.log("Sending request")
+  const db_res = await pool.query(query);
+  //console.log(db_res);
+  console.log(db_res.rows);
+  return res.status(200).json(db_res.rows);
+})
+
+const PORT = 3000
+app.listen(PORT, async () => {
+    console.log(`Server initalized on port ${PORT}`)
+    await pool.connect()
+    const res = await pool.query('SELECT $1::text as connected', ['Connection to postgres successful!']);
+    console.log(res.rows[0].connected);
 })
